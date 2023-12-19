087231f4577db628eefa799b5c1a83394c58e82c
johnvimalasir001 <johnvimalasir@gmail.com>
2023-12-19 21:24:56 +0530

    bloc project second commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:bloc_app/app_blocs.dart';
 import 'package:bloc_app/app_events.dart';
 import 'package:bloc_app/app_states.dart';
 import 'package:bloc_app/pages/Welcome/welcome.dart';
+import 'package:bloc_app/pages/welcome/bloc/welcome_blocs.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,25 +17,32 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AppBlocs(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AppBlocs(),
         ),
-        home: const WelcomePage(),
+        BlocProvider(
+          create: (context) => WelcomeBloc(),
+        ),
+      ],
+      child: ScreenUtilInit(
+        builder: (context, child) => MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: const WelcomePage(),
+          routes: {
+            "myHomePage": (context) => const MyHomePage(),
+          },
+        ),
       ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({
+    super.key,
+  });
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -45,7 +54,6 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
       ),
       body: Center(
         child: BlocBuilder<AppBlocs, AppStates>(
@@ -69,12 +77,14 @@ class _MyHomePageState extends State<MyHomePage> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           FloatingActionButton(
+            heroTag: "Hero1",
             onPressed: () =>
                 BlocProvider.of<AppBlocs>(context).add(Increment()),
             tooltip: 'Increment',
             child: const Icon(Icons.add),
           ),
           FloatingActionButton(
+            heroTag: "Hero2",
             onPressed: () =>
                 BlocProvider.of<AppBlocs>(context).add(Decrement()),
             tooltip: 'Increment',
--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -1,4 +1,10 @@
+import 'package:bloc_app/pages/welcome/bloc/welcome_blocs.dart';
+import 'package:bloc_app/pages/welcome/bloc/welcome_events.dart';
+import 'package:bloc_app/pages/welcome/bloc/welcome_states.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
@@ -8,10 +14,155 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+  PageController pageController = PageController(initialPage: 0);
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
+      body: BlocBuilder<WelcomeBloc, WelcomeState>(
+        builder: (context, state) {
+          return Container(
+            margin: EdgeInsets.only(top: 34.h),
+            width: 375.w,
+            child: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                PageView(
+                  controller: pageController,
+                  onPageChanged: (index) {
+                    state.page = index;
+                    BlocProvider.of<WelcomeBloc>(context).add(WelcomeEvent());
+                  },
+                  children: [
+                    _page(
+                      1,
+                      "assets/images/reading.png",
+                      "First See Learning",
+                      "Forget about a for of paper all knowledge in one learning",
+                      "Next",
+                      context,
+                    ),
+                    _page(
+                      2,
+                      "assets/images/boy.png",
+                      "Connect With Everyone",
+                      "Always keep in touch with your tutor & friend. Let's get connected!",
+                      "Next",
+                      context,
+                    ),
+                    _page(
+                      3,
+                      "assets/images/man.png",
+                      "Always Fascinated learning",
+                      "Anywhere, anytime. The time is at your discretion so study whenever you want.",
+                      "Get Started",
+                      context,
+                    ),
+                  ],
+                ),
+                Positioned(
+                  bottom: 50.h,
+                  child: DotsIndicator(
+                    dotsCount: 3,
+                    position: state.page,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    decorator: DotsDecorator(
+                      color: Colors.black.withOpacity(0.5),
+                      activeColor: Colors.blue,
+                      size: const Size.square(8),
+                      activeSize: const Size(18, 8),
+                      activeShape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _page(int index, String imgurl, String title, String subTitle,
+      String buttonName, BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 345.h,
+          width: 345.w,
+          child: Image.asset(
+            imgurl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        SizedBox(
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 24.sp,
+              fontWeight: FontWeight.normal,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        Container(
+          width: 375.w,
+          padding: EdgeInsets.only(top: 8.h, left: 25.w, right: 25.w),
+          child: Text(
+            subTitle,
+            style: TextStyle(
+              fontSize: 16.sp,
+              fontWeight: FontWeight.normal,
+              color: Colors.black.withOpacity(0.5),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            if (index < 3) {
+              pageController.animateToPage(index,
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.easeIn);
+            } else {
+              Navigator.of(context)
+                  .pushNamedAndRemoveUntil("myHomePage", (route) => false);
+            }
+          },
+          child: Container(
+            margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
+            width: 325.w,
+            height: 50.h,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.all(
+                Radius.circular(15.w),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 2,
+                  spreadRadius: 1,
+                  color: Colors.grey.withOpacity(0.1),
+                  offset: const Offset(0, 1),
+                ),
+              ],
+            ),
+            child: Center(
+              child: Text(
+                buttonName,
+                style: TextStyle(
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
